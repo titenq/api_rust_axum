5ed51649133a3d415df99203e0bf9a6c90070a9c
titenq <titenq@gmail.com>
2024-02-26 10:50:58 -0300

    mongodb

--- src/controllers/user_controller.rs
+++ src/controllers/user_controller.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 
 use crate::{
     libs::remove_accent,
-    structs::user_struct::{User, UserBody},
+    models::user_model::{User, UserBody},
 };
 use axum::{
     extract::{Path, Query},
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,56 @@
 mod routes;
-mod structs;
 mod controllers;
 mod libs;
+mod models;
 
-use axum::Router;
+mod db;
+mod error;
+mod handler;
+mod model;
+mod response;
+mod route;
+mod schema;
+
+use std::sync::Arc;
+
 use tokio::net::TcpListener;
+// use axum::{response::IntoResponse, routing::get, Json, Router};
+use axum::{http::{
+    header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE},
+    HeaderValue, Method,
+}, serve, Router};
+use db::DB;
+use dotenv::dotenv;
+use error::MyError;
+use route::create_router;
+use tower_http::cors::CorsLayer;
 
-use crate::routes::get_router;
+// use crate::routes::get_router;
+
+pub struct AppState {
+    db: DB,
+}
 
 #[tokio::main]
-async fn main() {
-    let app: Router = get_router();
+async fn main() -> Result<(), MyError> {
+    dotenv().ok();
+
+    let db: DB = DB::init().await?;
+    // let app: Router = get_router();
+
+    let cors: CorsLayer = CorsLayer::new()
+        .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
+        .allow_methods([Method::GET, Method::POST, Method::PATCH, Method::DELETE])
+        .allow_credentials(true)
+        .allow_headers([AUTHORIZATION, ACCEPT, CONTENT_TYPE]);
+
+    let app: Router = create_router(Arc::new(AppState { db: db.clone() })).layer(cors);
 
     println!("🚀 Server started successfully");
 
     let listener: TcpListener = TcpListener::bind("127.0.0.1:5000").await.unwrap();
 
-    axum::serve(listener, app).await.unwrap();
+    serve(listener, app).await.unwrap();
+
+    Ok(())
 }
